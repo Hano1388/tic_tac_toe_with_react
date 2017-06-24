0b574bcc15fa75ed214ff3cf2611ff26d4594514
Hano <hinsul3113@gmail.com>
2017-06-24 13:27:45 -0700

    Added state to squares

--- src/index.js
+++ src/index.js
@@ -6,13 +6,13 @@ class Square extends React.Component {
   constructor () {
     super();
     this.state = {
-      value = null,
+      value: null,
     };
   }
   render() {
     return (
-      <button className="square" onClick={ () => alert(`clicked ${this.props.value}`)}>
-        {this.props.value}
+      <button className="square" onClick={ () => this.setState({value: 'X'})}>
+        {this.state.value}
       </button>
     );
   }
